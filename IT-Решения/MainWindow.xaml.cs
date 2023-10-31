a9f37d4bedb2a1b26570a32af0ac17c9375219f9
lycontrope <bashkovgleb.com@gmail.com>
2023-10-31 23:42:04 +0300

    1.0.0

---
+++ IT-Решения/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IT_Решения
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Init(object sender, RoutedEventArgs e)//вода камни рыбы
+        {
+            login.Opacity = 0;
+            main.Opacity = 1;
+        }
+        private void Init1(object sender, RoutedEventArgs e)//вода камни рыбы
+        {
+            login.Opacity = 1;
+            main.Opacity = 0;
+        }
+    }
+}
